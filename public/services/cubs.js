88d60b7c1dbc910025ca5ac3acf201cc8a41c604
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-08-22 10:44:59 -0700

    working on some styling and components

--- public/services/cubs.js
+++ public/services/cubs.js
@@ -4,6 +4,7 @@ module.exports = [
         size: 'Large',
         weight: 165,
         friendly: false,
+        url: 'assets/jaguar.jpg',
         funFact: 'Jaguars have no defined breeding season and will mate any time of year.'
     },
     {
@@ -11,6 +12,7 @@ module.exports = [
         size: 'Medium',
         weight: 60,
         friendly: false,
+        url: 'assets/leopard.jpg',
         funFact: 'Leopards are mostly nocturnal, hunting prey at night.'
     },
     {
@@ -18,6 +20,7 @@ module.exports = [
         size: 'Large',
         weight: 72,
         friendly: false,
+        url: 'assets/snow.jpg',
         funFact: 'Believe it or not, snow leopards can’t roar.'
     },
     {
@@ -25,6 +28,7 @@ module.exports = [
         size: 'Extra-Large',
         weight: 330,
         friendly: false,
+        url: 'assets/lion.jpeg',
         funFact: 'A lion’s heels don’t touch the ground when it walks'
     },
     {
@@ -32,13 +36,15 @@ module.exports = [
         size: 'Large',
         weight: 103,
         friendly: false,
+        url: 'assets/cheetah.jpg',
         funFact: 'a cheetah can reach 112km/h in just three seconds.'
     },
     {
         name: 'Cougar',
         size: 'Large',
-        weight: 142
+        weight: 142,
         friendly: false,
+        url: 'assets/cougar.jpg',
         funFact: 'They actually hold the Guinness World Record for the animal with the highest number of names.'
     },
     {
@@ -46,6 +52,7 @@ module.exports = [
         size: 'Extra-Large',
         weight: 405,
         friendly: false,
+        url: 'assets/tiger.jpg',
         funFact: 'A tiger can consume up to 88 pounds of meat at one time.'
     },
     {
@@ -53,6 +60,7 @@ module.exports = [
         size: 'Small',
         weight: 6,
         friendly: true,
+        url: 'assets/sandcat.jpg',
         funFact: 'Thanks to the furry paws, sand cats don\'t leave footprints in the sand.'
     },
     {
@@ -60,6 +68,7 @@ module.exports = [
         size: 'Small',
         weight: 3,
         friendly: true,
+        url: 'assets/rusty.jpg',
         funFact: 'It has the nickname of the ‘hummingbird of the cat world’ because it is so agile.'
     },
     {
@@ -67,6 +76,7 @@ module.exports = [
         size: 'Medium',
         weight: 26,
         friendly: true,
+        url: 'assets/serval.jpg',
         funFact: 'A servals\' hind legs are longer than its front ones'
     },
     {
@@ -74,6 +84,7 @@ module.exports = [
         size: 'Medium',
         weight: 26,
         friendly: false,
+        url: 'assets/caracal.jpg',
         funFact: 'Chinese emperors used caracals as gifts.'
     },
 ];
---
+++ public/src/components/cubs/CubItem.js
@@ -0,0 +1,11 @@
+import Component from '../Component.js';
+
+class CubItem extends Component {
+    renderHTML() {
+        return /*html*/`
+            
+        `;
+    }
+}
+
+export default CubItem;
